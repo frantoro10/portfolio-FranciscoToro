05dac0bf96bdced6f7475013893cb8d5e63a2463
Francisco <torofrancisco13@gmail.com>
2025-06-24 14:33:37 -0300

    adding new projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,30 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
+      title: 'Task manager',
+      img: '/projects/task-manager.png',
+      description: 'Task Manager is a minimal full-stack application to help you organize your daily tasks efficiently. It features a clean and modern interface built with Next.js, TypeScript, TailwindCSS and a simple Express.js backend. Tasks are stored in a local JSON file, making it ideal for educational purposes or quick demos. For personal or production use, I recommend integrating a proper database for data persistence.',
+      tec1: 'TypeScript',
+      tec2: 'Next.js',
+      tec3: 'Express.js',
+      github: 'https://github.com/frantoro10/task-manager',
+      url: 'https://task-manager-1y38.vercel.app/',
+    },
+    {
+      id: 2,
+      title: 'Two automated bots: Binance Futures Tranding and World of Warcraft Auction House data analysis', 
+      img: '/projects/automated_bot.jpg',
+      description:
+        'A Python project featuring two automation bots: one for algorithmic trading on Binance Futures using technical indicators and risk management, and another for searching and analyzing item deals in the World of Warcraft Auction House via the Blizzard API. Both bots are fully configurable and designed for efficiency and ease of use.',
+      tec1: "Python",
+      tec2: "Binance API",
+      tec3: "Blizzard API",  
+      github: 'https://github.com/frantoro10/automated-bots-python',
+      url: 'https://github.com/frantoro10/automated-bots-python',
+    }, 
+
+    {
+      id: 3,
       title: 'E-commerce',
       img: '/projects/ecommerce-react.jpg',
       description:
@@ -15,7 +39,7 @@ const Projects = () => {
       url: 'https://ecommerce-react-js-umber.vercel.app/',
     },
     {
-      id: 2,
+      id: 4,
       title: 'Mini Business System',
       img: '/projects/mini-business-app.jpg',
       description:
@@ -27,15 +51,15 @@ const Projects = () => {
       url: 'https://mini-business.vercel.app/',
     },
     {
-        id: 3,
-        title: 'investment application ',
-        img: '/projects/github-logo.webp',
-        description: 'ARGBroker is a console-based stock investment application that allows users to simulate buying and selling stocks, visualize their portfolio, and manage user accounts in a simple, educational environment.',
-        tec1: "Python",
-        tec2: "MySQL",
-        tec3: "DAO",
-        github: 'https://github.com/frantoro10/project-tecnicatura-argbroker',
-        url: 'https://github.com/frantoro10/project-tecnicatura-argbroker',
+      id: 5,
+      title: 'investment application ',
+      img: '/projects/github-logo.webp',
+      description: 'ARGBroker is a console-based stock investment application that allows users to simulate buying and selling stocks, visualize their portfolio, and manage user accounts in a simple, educational environment.',
+      tec1: "Python",
+      tec2: "MySQL",
+      tec3: "DAO",
+      github: 'https://github.com/frantoro10/project-tecnicatura-argbroker',
+      url: 'https://github.com/frantoro10/project-tecnicatura-argbroker',
     }
   ];
 
